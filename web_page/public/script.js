060a6c1d98771dc098c9b01f2d0084f770550133
umeow0716 <jj30462281@gmail.com>
2024-06-30 16:51:56 +0800

    webpage background-color change & button style(shadow)

--- web_page/public/script.js
+++ web_page/public/script.js
@@ -1,37 +1,67 @@
 const url = new URL(window.location.href)
 const ip = url.host.split(':')[0]
 
-const ws = new WebSocket(`ws://${ip}:8765`);
+let ws = new WebSocket(`ws://${ip}:8765`);
 
-ws.onmessage = (event) => {
-  console.log(event)
-}
+let isTrust = false
 
 setInterval(keep_alive, 1000)
 
-function movement(ele, data) {
-  ws.send(JSON.stringify({
-    type: 'movement',
-    data
-  }))
+function movement(ele, event, data) {
+  try {
+    ws.send(JSON.stringify({
+      type: 'movement',
+      data
+    }))
+  } catch(err) {
+    console.error(err)
+  }
   ele?.classList?.add("button-touched")
 
-  const audio = document.createElement("audio");
-  audio.src = "wooden_fish.mp3";
-  audio.play();
-  navigator.vibrate(20);
+  if(!isTrust) return
+
+  const audio = document.createElement("audio")
+  audio.src = `${data}.mp3`
+  audio.play()
+  plus_one(event)
+  window.navigator.vibrate(20)
 }
 
 function movement_touchend(ele) {
-  ws.send(JSON.stringify({
-    type: 'movement',
-    data: 'stop'
-  }))
+  try {
+    ws.send(JSON.stringify({
+      type: 'movement',
+      data: 'stop'
+    }))
+  } catch(err) {
+    console.error(err)
+  }
   ele.classList.remove("button-touched")
+  isTrust = true
+}
+
+function plus_one(event) {
+  console.log(event)
+  let x = event.targetTouches[0].clientX + 10;
+  let y = event.targetTouches[0].clientY - 30;
+
+  let element = document.createElement('span')
+  element.innerHTML = '功德 +1'
+  element.setAttribute("style", `position: fixed;top: ${y}px;left: ${x}px;`)
+  document.body.appendChild(element)
+  element.setAttribute("class", "plus_one")
+
+  setTimeout(() => {
+    element.remove()
+  }, 900)
 }
 
 function keep_alive() {
-  ws.send(JSON.stringify({
-    type: 'ping'
-  }))
+  try{
+    ws.send(JSON.stringify({
+      type: 'ping'
+    }))
+  } catch(err) {
+    ws = new WebSocket(`ws://${ip}:8765`);
+  }
 }
