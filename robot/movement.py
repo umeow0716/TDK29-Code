66d61a511c436713802d17c75f480c5ad5c819ef
umeow0716 <jj30462281@gmail.com>
2024-08-04 15:29:33 +0800

    connect to Arduino

--- robot/movement.py
+++ robot/movement.py
@@ -1,72 +1,66 @@
+from .arduino.arduino_motor import ArduinoMotor
 from .raspberry.motor import Motor
 
 class Movement:
-    right_ahead_wheel = Motor(24, 23, 21)
-    right_rear_wheel = Motor(26, 19, 20)
-
-    left_ahead_wheel = Motor(27, 17, 12)
-    left_rear_wheel = Motor(5, 6, 13)
+    right_wheel = ArduinoMotor(22, 23, 2)
+    left_wheel = ArduinoMotor(24, 25, 3)
     
-    right_ahead_wheel.stop()
-    right_rear_wheel.stop()
-    left_ahead_wheel.stop()
-    left_rear_wheel.stop()
+    right_wheel.stop()
+    left_wheel.stop()
     
     @staticmethod
     def execute(direction):
         print("movement:", direction)
         if direction == 'forward':
             # wheel process...
-            Movement.right_ahead_wheel.forward()
-            Movement.right_rear_wheel.forward()
-            Movement.left_ahead_wheel.forward()
-            Movement.left_rear_wheel.forward()
+            Movement.right_wheel.forward()
+            Movement.left_wheel.forward()
             pass
 
         if direction == 'backward':
             # wheel process...
-            Movement.right_ahead_wheel.backward()
-            Movement.right_rear_wheel.backward()
-            Movement.left_ahead_wheel.backward()
-            Movement.left_rear_wheel.backward()
+            Movement.right_wheel.backward()
+            Movement.left_wheel.backward()
             pass
 
-        # if direction == 'right':
-        #     # wheel process...
-        #     right_ahead_wheel.backward()
-        #     right_rear_wheel.backward()
-        #     left_ahead_wheel.forward()
-        #     left_rear_wheel.forward()
-        #     pass
-
-        if direction == 'right':  #麥輪
+        if direction == 'right':
             # wheel process...
-            Movement.right_ahead_wheel.backward()
-            Movement.right_rear_wheel.forward()
-            Movement.left_ahead_wheel.forward()
-            Movement.left_rear_wheel.backward()
+            Movement.left_wheel.forward()
             pass
 
-        # if direction == 'left':
+        # if direction == 'right':  #麥輪
         #     # wheel process...
-        #     right_ahead_wheel.forward()
-        #     right_rear_wheel.forward()
-        #     left_ahead_wheel.backward()
-        #     left_rear_wheel.backward()
+        #     Movement.right_ahead_wheel.backward()
+        #     Movement.right_rear_wheel.forward()
+        #     Movement.left_ahead_wheel.forward()
+        #     Movement.left_rear_wheel.backward()
         #     pass
 
-        if direction == 'left':   #麥輪
+        if direction == 'left':
             # wheel process...
-            Movement.right_ahead_wheel.forward()
-            Movement.right_rear_wheel.backward()
-            Movement.left_ahead_wheel.backward()
-            Movement.left_rear_wheel.forward()
+            Movement.right_wheel.forward()
+            pass
+
+        # if direction == 'left':   #麥輪
+        #     # wheel process...
+        #     Movement.right_ahead_wheel.forward()
+        #     Movement.right_rear_wheel.backward()
+        #     Movement.left_ahead_wheel.backward()
+        #     Movement.left_rear_wheel.forward()
+        #     pass
+
+        if direction == 'clockwise':
+            Movement.right_wheel.backward()
+            Movement.left_wheel.forward()
+            pass
+            
+        if direction == 'counterclockwise':
+            Movement.right_wheel.forward()
+            Movement.left_wheel.backward()
             pass
 
         if direction == 'stop':
             # wheel process...
-            Movement.right_ahead_wheel.stop()
-            Movement.right_rear_wheel.stop()
-            Movement.left_ahead_wheel.stop()
-            Movement.left_rear_wheel.stop()
+            Movement.right_wheel.stop()
+            Movement.left_wheel.stop()
             pass
